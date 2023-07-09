041b896e9259e881cef0447058ad5d229722dbe6
jmindeveloper <jmin.developer@gmail.com>
2023-07-09 17:00:29 +0900

    project 생성

---
+++ Grouping/Grouping/GroupingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupingApp.swift
+//  Grouping
+//
+//  Created by J_Min on 2023/07/09.
+//
+
+import SwiftUI
+
+@main
+struct GroupingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
