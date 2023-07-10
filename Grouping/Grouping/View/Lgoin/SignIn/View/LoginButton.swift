656d527c1ce954795fd334faf6ec26f72f33120c
jmindeveloper <jmin.developer@gmail.com>
2023-07-10 20:28:13 +0900

    LoginButton view

--- Grouping/Grouping/View/Lgoin/SignIn/View/LoginButton.swift
+++ Grouping/Grouping/View/Lgoin/SignIn/View/LoginButton.swift
@@ -5,4 +5,62 @@
 //  Created by J_Min on 2023/07/10.
 //
 
-import Foundation
+import SwiftUI
+
+struct LoginButton: View {
+    var backgroundColor: Color
+    var imageName: String?
+    var title: String
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                if let name = imageName {
+                    Image(name)
+                        .resizable()
+                        .frame(width: 45, height: 45)
+                        .scaledToFill()
+                        .clipShape(Circle())
+                        .padding(.leading, 16)
+                }
+                
+                Spacer()
+            }
+            
+            HStack {
+                Spacer()
+                
+                Text(title)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+            }
+        }
+        .frame(height: 56)
+        .background(
+            RoundedRectangle(cornerRadius: 20)
+                .fill(backgroundColor)
+        )
+    }
+    
+    func backgroundColor(_ color: Color) -> LoginButton {
+        LoginButton(backgroundColor: color, imageName: imageName, title: title)
+    }
+    
+    func title(_ title: String) -> LoginButton {
+        LoginButton(backgroundColor: backgroundColor, imageName: imageName, title: title)
+    }
+    
+    func image(_ imageName: String?) -> LoginButton {
+        LoginButton(backgroundColor: backgroundColor, imageName: imageName, title: title)
+    }
+}
+
+struct LoginButton_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginButton(backgroundColor: Color(uc: .clear), imageName: "test_image", title: "구글로 로그인하기")
+            .backgroundColor(.red)
+            .previewLayout(.sizeThatFits)
+            .frame(width: Constant.screenWidth - 32, height: 56)
+    }
+}
