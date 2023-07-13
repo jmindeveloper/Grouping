d0c987443ab205b1609abd285b2f9293bb1f297e
jmindeveloper <jmin.developer@gmail.com>
2023-07-13 22:26:51 +0900

    post upload 구현

--- Grouping/Grouping/Manager/Post/PostManagementManager.swift
+++ Grouping/Grouping/Manager/Post/PostManagementManager.swift
@@ -10,74 +10,120 @@ import FirebaseFirestore
 import FirebaseStorage
 
 protocol PostManagementManagerInterface {
-    func upload(completion: ((_ post: Post) -> Void)?)
-    func update(completion: ((_ post: Post) -> Void)?)
+    func upload(images: [Data], content: String, location: Location?, tags: [String], completion: ((_ post: Post) -> Void)?)
+    func update(post: Post, images: [Data]?, content: String?, location: Location?, tags: [String]?, completion: ((_ post: Post) -> Void)?)
     func delete(completion: ((_ isSuccess: Bool) -> Void)?)
-    func createPost(images: [String], content: String, location: Location, tags: [String]) -> Post?
-    func editPost(post: Post, images: [String]?, content: String?, location: Location?, tags: [String]?) -> Post?
 }
 
 final class PostManagementManager: PostManagementManagerInterface {
     private let ref = Storage.storage().reference()
-    private let db = Firestore.firestore()
+    private let db = Firestore.firestore().collection(FBFieldName.users)
     
-    func createPost(
-        images: [String],
+    //    func createPost(
+    //        images: [String],
+    //        content: String,
+    //        location: Location,
+    //        tags: [String]
+    //    ) -> Post? {
+    //        guard let user = UserAuthManager.shared.user else {
+    //            return nil
+    //        }
+    //
+    //        return Post(
+    //            id: UUID().uuidString,
+    //            createUserId: user.id,
+    //            images: images,
+    //            content: content,
+    //            createdAt: Date(),
+    //            updatedAt: nil,
+    //            location: location,
+    //            heartCount: 0,
+    //            heartUsers: [],
+    //            commentCount: 0,
+    //            tags: tags
+    //        )
+    //    }
+    //
+    //    func editPost(
+    //        post: Post,
+    //        images: [String]?,
+    //        content: String?,
+    //        location: Location?,
+    //        tags: [String]?
+    //    ) -> Post? {
+    //        guard let user = UserAuthManager.shared.user,
+    //              user.id == post.createUserId else {
+    //            return nil
+    //        }
+    //
+    //        return Post(
+    //            id: post.id,
+    //            createUserId: post.createUserId,
+    //            images: images == nil ? post.images : images!,
+    //            content: content == nil ? post.content : content!,
+    //            createdAt: post.createdAt,
+    //            updatedAt: Date(),
+    //            location: location == nil ? post.location : location!,
+    //            heartCount: post.heartCount,
+    //            heartUsers: post.heartUsers,
+    //            commentCount: post.commentCount,
+    //            tags: tags == nil ? post.tags : tags!
+    //        )
+    //    }
+    
+    func upload(
+        images: [Data],
         content: String,
-        location: Location,
-        tags: [String]
-    ) -> Post? {
+        location: Location?,
+        tags: [String],
+        completion: ((_ post: Post) -> Void)? = nil
+    ) {
         guard let user = UserAuthManager.shared.user else {
-            return nil
+            return
         }
         
-        return Post(
-            id: UUID().uuidString,
-            createUserId: user.id,
-            images: images,
-            content: content,
-            createdAt: Date(),
-            updatedAt: nil,
-            location: location,
-            heartCount: 0,
-            heartUsers: [],
-            commentCount: 0,
-            tags: tags
-        )
+        uploadImages(images: images) { totalCount, doneCount in
+            print("uploadCount --> ", totalCount, doneCount)
+        } completion: { [weak self] urlStrings in
+            guard let self = self else { return }
+            let post =  Post(
+                id: UUID().uuidString,
+                createUserId: user.id,
+                images: urlStrings,
+                content: content,
+                createdAt: Date(),
+                updatedAt: nil,
+                location: location,
+                heartCount: 0,
+                heartUsers: [],
+                commentCount: 0,
+                tags: tags
+            )
+            
+            do {
+                try self.db.document(user.id)
+                    .collection(FBFieldName.post)
+                    .document(post.id)
+                    .setData(from: post) { error in
+                        guard error == nil else {
+                            print(error!.localizedDescription)
+                            return
+                        }
+                        completion?(post)
+                    }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
     
-    func editPost(
+    func update(
         post: Post,
-        images: [String]?,
         content: String?,
         location: Location?,
-        tags: [String]?
-    ) -> Post? {
-        guard let user = UserAuthManager.shared.user,
-              user.id == post.createUserId else {
-            return nil
-        }
-        
-        return Post(
-            id: post.id,
-            createUserId: post.createUserId,
-            images: images == nil ? post.images : images!,
-            content: content == nil ? post.content : content!,
-            createdAt: post.createdAt,
-            updatedAt: Date(),
-            location: location == nil ? post.location : location!,
-            heartCount: post.heartCount,
-            heartUsers: post.heartUsers,
-            commentCount: post.commentCount,
-            tags: tags == nil ? post.tags : tags!
-        )
-    }
-    
-    func upload(completion: ((_ post: Post) -> Void)? = nil) {
-        
-    }
-    
-    func update(completion: ((_ post: Post) -> Void)? = nil) {
+        tags: [String]?,
+        completion: ((_ post: Post) -> Void)? = nil
+    ) {
         
     }
     
@@ -85,7 +131,7 @@ final class PostManagementManager: PostManagementManagerInterface {
         
     }
     
-    func uploadImages(
+    private func uploadImages(
         images: [Data],
         uploadDoneCount: ((_ totalCount: Int, _ doneCount: Int) -> Void)? = nil,
         completion: ((_ urlStrings: [String]) -> Void)? = nil
