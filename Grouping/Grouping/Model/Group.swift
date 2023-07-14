6a5e5ac6dfdd558c6216c575524fd8dffbee99d6
jmindeveloper <jmin.developer@gmail.com>
2023-07-14 20:46:57 +0900

    GroupManagementManager 정의 및 구현

--- Grouping/Grouping/Model/Group.swift
+++ Grouping/Grouping/Model/Group.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Group {
+struct Group: Codable {
     /// 그룹 id
     let groupId: String
     /// group name
@@ -19,7 +19,7 @@ struct Group {
     /// 그룹에 포함된 Post 리스트
     var posts: [String]
     /// 그룹 만든사람
-    var createUser: String
+    var createUserId: String
     /// 그룹 관리하는 유저들
     var managementUsers: [String]
     /// 그룹에 가입된 사람들(createUser 포함)
