e377bf7f1dd1b0e8015e19cc00fa77c38632f1cb
jmindeveloper <jmin.developer@gmail.com>
2023-07-19 23:16:01 +0900

    ProfileEditView image, name

--- Grouping/Grouping/View/Profile/ProfileEdit/ProfileEditView.swift
+++ Grouping/Grouping/View/Profile/ProfileEdit/ProfileEditView.swift
@@ -9,6 +9,40 @@ import SwiftUI
 
 struct ProfileEditView: View {
     var body: some View {
-        Text("프로필 편집")
+        ScrollView {
+            VStack {
+                Image("test_image_5")
+                    .resizable()
+                    .frame(width: 160, height: 160)
+                    .clipShape(Circle())
+                    .padding(.top, 16)
+                
+                Text("김지민")
+                    .font(.system(size: 31, weight: .semibold))
+                    .padding(.top, 10)
+                
+                Divider()
+            }
+        }
+        .navigationTitle("프로필 편집")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Text("완료")
+                        .foregroundColor(.primary)
+                }
+            }
+        }
+    }
+}
+
+struct ProfileEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ProfileEditView()
+        }
     }
 }
