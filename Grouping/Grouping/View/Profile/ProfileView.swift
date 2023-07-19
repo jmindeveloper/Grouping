d488e157c67ec235db9cf4c9a2b16fcfd18f5ef4
jmindeveloper <jmin.developer@gmail.com>
2023-07-19 21:31:21 +0900

    profile user info header ui 구현

--- Grouping/Grouping/View/Profile/ProfileView.swift
+++ Grouping/Grouping/View/Profile/ProfileView.swift
@@ -20,10 +20,85 @@ struct ProfileView<VM>: View where VM: ProfileViewModelInterface {
     
     var body: some View {
         ScrollView {
+            userInfoView()
+            
             albumGrid()
         }
         .navigationTitle(viewModel.user?.nickName ?? "Profile")
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    @ViewBuilder
+    private func userInfoView() -> some View {
+        VStack(spacing: 0) {
+            HStack {
+                Image("test_image_5")
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .clipShape(Circle())
+                    .padding(.leading, 16)
+                
+                Spacer()
+                
+                HStack {
+                    bottomTitleTopValueView(title: "게시물", value: "56")
+                        .padding(.horizontal, 6)
+                    bottomTitleTopValueView(title: "팔로워", value: "282")
+                        .padding(.horizontal, 6)
+                    bottomTitleTopValueView(title: "팔로잉", value: "455")
+                        .padding(.horizontal, 6)
+                }
+                
+                Spacer()
+            }
+            
+            Button {
+                
+            } label: {
+                Text("프로필 편집")
+                    .foregroundColor(.white)
+            }
+            .frame(width: Constant.screenWidth - 32, height: 35)
+            .background(RoundedRectangle(cornerRadius: 14).fill(Color(uc: .systemGray2)))
+            .padding(.top)
+            
+            HStack {
+                Button {
+                    
+                } label: {
+                    Text("프로필 편집")
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: Constant.screenWidth - 32, minHeight: 35)
+                .background(RoundedRectangle(cornerRadius: 14).fill(Color(uc: .systemGray2)))
+                .padding(.top)
+                
+                Button {
+                    
+                } label: {
+                    Text("프로필 편집")
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: Constant.screenWidth - 32, minHeight: 35)
+                .background(RoundedRectangle(cornerRadius: 14).fill(Color(uc: .systemGray2)))
+                .padding(.top)
+            }
+            .padding(.horizontal, 16)
+            
+            Divider()
+                .padding(.top, 16)
+        }
+    }
+    
+    @ViewBuilder
+    private func bottomTitleTopValueView(
+        title: String,
+        value: String
+    ) -> some View {
+        VStack {
+            Text(value)
+            Text(title)
+        }
     }
     
     @ViewBuilder
@@ -43,5 +118,11 @@ struct ProfileView<VM>: View where VM: ProfileViewModelInterface {
                 .buttonStyle(.plain)
             }
         }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView(viewModel: ProfileViewModel())
     }
 }
