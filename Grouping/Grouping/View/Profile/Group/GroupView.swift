198723f4b32e65739d9d8516c288825e3653e9c7
jmindeveloper <jmin.developer@gmail.com>
2023-07-23 16:40:58 +0900

    groupview scrollview and dismiss button

--- Grouping/Grouping/View/Profile/Group/GroupView.swift
+++ Grouping/Grouping/View/Profile/Group/GroupView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct GroupView: View {
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        Text("Hello, Group!!!")
+        ZStack {
+            ScrollView {
+                Color.red
+                    .frame(height: 10000)
+            }
+            .ignoresSafeArea(edges: [.all])
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 18, height: 18)
+                            .foregroundColor(.primary)
+                    }
+                    .frame(width: 44, height: 44)
+                    .background(Circle().fill(Color(uiColor: .systemGray3)))
+                    .shadow(radius: 5, x: 0, y: 5)
+                    .contentShape(Rectangle())
+                    
+                    
+                }
+                .padding(.trailing, 18)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct GroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupView()
     }
 }
