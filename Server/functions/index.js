6661910ac02b8f64ebd180e55180698572ab9d64
jmindeveloper <jmin.developer@gmail.com>
2023-11-12 03:53:18 +0900

    getUse API Test

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -1,19 +1,54 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
+const app = express();
+const groupingApp = express.Router()
+
+const db = admin.firestore();
+
+app.use(express.json());
+app.use("/api", groupingApp);
+
+exports.groupingApp = functions.region("asia-northeast3").https.onRequest(app);
+
+// groupingApp.get("/users", async (req, res) => {
+//     try {
+//         const userDoc = await db
+//             .collection("Users")
+//             .doc(req.query.userId)
+//             .get();
+
+//             res.status(200).send({ id: userDoc.id })
+//     } catch (error) {
+//         console.log(error)
+//         res.status(400).send("유저정보를 가져오는데 실패했습니다.");
+//     }
 // });
+
+groupingApp.get("/users", async (req, res) => {
+    try {
+        const userId = req.query.userId;
+
+        if (!userId) {
+            return res.status(400).send("userId가 제공되지 않았습니다.");
+        }
+
+        const userDoc = await db
+            .collection("Users")
+            .doc(userId)
+            .get();
+
+        if (!userDoc.exists) {
+            res.send(404).send("유저정보를 가져오는데 실패했습니다");
+            return
+        }
+
+        res.status(200).send({ id: userDoc.id, ...userDoc.data() });
+    } catch (error) {
+        res.status(400).send("유저정보를 가져오는데 실패했습니다: ${error.message}");
+    }
+});
