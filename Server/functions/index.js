8cb95f61646d8295046e94809a86f9b54c06023c
jmindeveloper <jmin.developer@gmail.com>
2023-11-12 17:18:41 +0900

    create User API

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -15,20 +15,6 @@ app.use("/api", groupingApp);
 
 exports.groupingApp = functions.region("asia-northeast3").https.onRequest(app);
 
-// groupingApp.get("/users", async (req, res) => {
-//     try {
-//         const userDoc = await db
-//             .collection("Users")
-//             .doc(req.query.userId)
-//             .get();
-
-//             res.status(200).send({ id: userDoc.id })
-//     } catch (error) {
-//         console.log(error)
-//         res.status(400).send("유저정보를 가져오는데 실패했습니다.");
-//     }
-// });
-
 groupingApp.get("/users", async (req, res) => {
     try {
         const userId = req.query.userId;
@@ -50,5 +36,36 @@ groupingApp.get("/users", async (req, res) => {
         res.status(200).send({ id: userDoc.id, ...userDoc.data() });
     } catch (error) {
         res.status(400).send("유저정보를 가져오는데 실패했습니다: ${error.message}");
+    }
+});
+
+groupingApp.post("/users", async (req, res) => {
+    try {
+        const userId = req.body.id;
+
+        if (!userId) {
+            return res.status(404).send("userId가 잘못됐습니다.");
+        }
+
+        const user = {
+            id: req.body.id,
+            nickName: req.body.nickName,
+            followers: req.body.followers,
+            following: req.body.following,
+            email: req.body.email,
+            profileImagePath: req.body.profileImagePath || null,
+            birthDay: req.body.birthDay || null,
+            phoneNumber: req.body.phoneNumber || null,
+            gender: req.body.gender || null,
+        };
+
+        await db
+            .collection("Users")
+            .doc(userId)
+            .set(user);
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).send(`유저를 생성하는데 실패했습니다: ${error.message}`);
     }
 });
